fc63345e69d7c788a5a9d24b4b9b15a54646a2d7
M-BATISTA09 <hnmarcos244@gmail.com>
2024-08-29 20:32:47 -0300

    Create exec-1.py

---
+++ exec-1.py
@@ -0,0 +1,6 @@
+'''
+1. Frase na tela - Implemente um programa que escreve na tela a frase "O primeiro programa a gente nunca esquece!".p
+
+'''
+
+print("O primeiro programa a gente nunca esquece!")
