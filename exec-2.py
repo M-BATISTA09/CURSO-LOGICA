f70ad1244df64000a81d6f3f12a7a9dffdfea0f3
M-BATISTA09 <hnmarcos244@gmail.com>
2024-08-29 20:54:02 -0300

    Update exec-2.py

--- exec-2.py
+++ exec-2.py
@@ -1,1 +1,6 @@
 
+#2. Etiqueta - Elabore um programa que escreve seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+
+print("Marcos")
+print("Rio de Janeiro")
+print("Duque de Caxias")
