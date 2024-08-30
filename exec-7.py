68ba0609fb1fedfbe9b5945170c4980ec23376e4
M-BATISTA09 <hnmarcos244@gmail.com>
2024-08-29 21:47:44 -0300

    Create exec-7.py

---
+++ exec-7.py
@@ -0,0 +1,26 @@
+#7. Pinheiro  - Implemente um programa que desenhe um "pinheiro" na tela, similar ao abaixo.
+#Enriqueça o desenho com outros caracteres, simulando enfeites.
+      # X
+     # XXX
+    # XXXXX
+   # XXXXXXX
+  # XXXXXXXXX
+ # XXXXXXXXXXX
+# XXXXXXXXXXXXX
+#xXXXXXXXXXXXXXX
+      #XX
+     # XX
+     #XXX
+  
+      
+print("        x ")
+print("       xxx ")
+print("      xxxxx  ")
+print("     xxxxxxx  ")
+print("    xxxxxxxxx  ")
+print("   xxxxxxxxxxx  ")
+print("  xxxxxxxxxxxxx  ")
+print(" xxxxxxxxxxxxxxx ")
+print("       xx      ")
+print("       xx       ")
+print("      xxxx     ")
