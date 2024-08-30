81725e9c0a51f9374248c797482d7c3ace235dfe
M-BATISTA09 <hnmarcos244@gmail.com>
2024-08-29 21:47:20 -0300

    Create exec-6.py

---
+++ exec-6.py
@@ -0,0 +1,16 @@
+#6. Menu - Elabore um programa que mostre o seguinte menu na tela:Cadastro de Clientes
+
+
+#0 - Fim
+#1 - Inclui
+#2 - Altera
+#3 - Exclui
+#4 - Consulta
+
+
+
+print("fim")
+print("incluir")
+print("alterar")
+print("excluir")
+print("consultar")
