9f9916b933c14175c545c52d0974d3dd5699708b
M-BATISTA09 <hnmarcos244@gmail.com>
2024-08-29 21:00:30 -0300

    Create exec-3.py

---
+++ exec-3.py
@@ -0,0 +1,3 @@
+#3. L Faça um programa que mostre na tela uma letra de música que você gosta (proibido letras do Justin Bieber).
+
+print("Baby, if you give it to me, I'll give it to you I know what you want")
